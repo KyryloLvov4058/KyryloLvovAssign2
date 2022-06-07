ded30a68aab176c79a1e802cc16f6a633a4ce7b1
kyrylolvov <kyrylo@arccompute.io>
2022-06-07 13:31:11 -0400

    ADDED: initial commit

---
+++ KyryloLab22/src/main/java/kyrylo/lvov/n01414058/KyryloActivity.java
@@ -0,0 +1,15 @@
+// Kyrylo Lvov n01414058 section B
+package kyrylo.lvov.n01414058;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KyryloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
